4cd9cc47d86317a44680987604acea4569da01a2
Carlos Silva <2110840@my.ipleiria.pt>
2015-01-04 11:16:08 +0000

    Revert "4 commit"
    This reverts commit 68a28cb52f55fbec64a78bf7a8794cd8b178d018.

--- BookStroreApplication/FormMain.cs
+++ BookStroreApplication/FormMain.cs
@@ -106,7 +106,7 @@ namespace BookStroreApplication
                             foreach (Book b in books)
                             {
                                 txtBox_OutPut.Text += printBook(b) + Environment.NewLine;
-                                //teste ulçtimo commitgeg
+                                //teste ulçtimo commit
                             }
                             break;
                         default:
